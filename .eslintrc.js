2b4e0217d8c7ac56fa7f2c9dc147cd42499165d0
Sathiyasri <sathiyasris.cse2024@citchennai.net>
2026-05-04 16:11:48 +0530

    Updated UI and fixed features

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,3 +16,4 @@ module.exports =
     "no-underscore-dangle": 0
   }
 }
+//eslintrc.js file
--- backend/server.js
+++ backend/server.js
@@ -35,3 +35,4 @@ app.get('*', (req, res) => {
 app.listen(config.PORT, () => {
   console.log('Server started at http://localhost:5000');
 });
+//server.js
